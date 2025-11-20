880df4ca8a7671532c0befeb2147bd1863144d2e
UdaySingh-281 <udaysinghspn28122002@gmail.com>
2025-11-21 01:48:44 +0530

    Updated Files

--- app.py
+++ app.py
@@ -11,11 +11,6 @@ cursor = db.cursor()
 
 
 
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-
 @app.route('/submit', methods=['POST'])
 def submit():
     if request.method == 'POST':
